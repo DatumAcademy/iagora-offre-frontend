3fc48398edb4cd536f39a8aec22ea5be59ef0bcd
Nick Mathieu <rakotomanananick29@gmail.com>
2024-10-09 21:05:20 +0300

    [Frontend] Ajout d'un loader

--- src/layouts/sections/navigation/nav-tabs/components/TabsSimple/index.js
+++ src/layouts/sections/navigation/nav-tabs/components/TabsSimple/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Container, Grid, AppBar, TextField, Card, Typography } from "@mui/material";
+import { Container, Grid, AppBar, TextField, Card, Typography, CircularProgress } from "@mui/material";
 import Pagination from "@mui/material/Pagination";
 import axios from "axios";
 import { API_URL } from "constants/coonstant.";
@@ -10,15 +10,17 @@ function StudentApplications() {
   const [offers, setOffers] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(true);
   const [searchParams, setSearchParams] = useState({
     label: "",
     skills: "",
     type: ""
   });
 
-  const numETU  = localStorage.getItem("numETU");
+  const numETU = localStorage.getItem("numETU");
 
   const fetchApplications = async () => {
+    setLoading(true);
     try {
       const response = await axios.get(`${API_URL}/student/${numETU}`, {
         params: {
@@ -31,6 +33,8 @@ function StudentApplications() {
       setTotalPages(data.totalPages);
     } catch (error) {
       console.error("Erreur lors du chargement des candidatures :", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -58,61 +62,85 @@ function StudentApplications() {
     <Container>
       <Grid container spacing={3} justifyContent="center">
         <Grid item xs={12} lg={10}>
-          <AppBar position="static">
-          </AppBar>
-            <Grid container spacing={2}>
-              <Grid item xs={12} md={3}>
-                <TextField
-                  label="Titre du poste"
-                  name="label"
-                  fullWidth
-                  value={searchParams.label}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={12} md={3}>
-                <TextField
-                  label="Compétences (séparées par des virgules)"
-                  name="skills"
-                  fullWidth
-                  value={searchParams.skills}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={12} md={3}>
-                <TextField
-                  label="Type"
-                  name="type"
-                  fullWidth
-                  value={searchParams.type}
-                  onChange={handleInputChange}
-                />
-              </Grid>
+          <AppBar position="static"></AppBar>
+
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={3}>
+              <TextField
+                label="Titre du poste"
+                name="label"
+                fullWidth
+                value={searchParams.label}
+                onChange={handleInputChange}
+              />
             </Grid>
-          <Grid container spacing={2} sx={{ mt: 2 }}>
-            {offers.map((offer) => (
-              <Grid item xs={12} md={6} lg={4} key={offer.id}>
-                <Card sx={{ p: 2 }} onClick={() => handleCardClick(offer.id)}>
-                  <Typography variant="h6">{offer.label}</Typography>
-                  <hr></hr>
-                  <Typography variant="body2"><strong>Entreprise :</strong> {offer.company}</Typography>
-                  <Typography variant="body2"><strong>Compétences :</strong> {offer.skills}</Typography>
-                  <Typography variant="body2"><strong>Type de contrat :</strong> {offer.contract}</Typography>
-                  <Typography variant="body2"><strong>Ville :</strong> {offer.city}</Typography>
-                  <Typography variant="body2"><strong>Date de publication :</strong> {offer.publicationdate}</Typography>
-                  <Typography variant="body2"><strong>Date limite :</strong> {offer.deadlinedate}</Typography>
-                  <Typography variant="body2"><strong>Nombre de candidats :</strong> {offer.candidate ? offer.candidate.length : 0}</Typography>
-                </Card>
-              </Grid>
-            ))}
+            <Grid item xs={12} md={3}>
+              <TextField
+                label="Compétences (séparées par des virgules)"
+                name="skills"
+                fullWidth
+                value={searchParams.skills}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <TextField
+                label="Type"
+                name="type"
+                fullWidth
+                value={searchParams.type}
+                onChange={handleInputChange}
+              />
+            </Grid>
           </Grid>
-          <Pagination
-            count={totalPages}
-            page={page}
-            onChange={handlePageChange}
-            sx={{ mt: 4, display: "flex", justifyContent: "center" }}
-            color="primary"
-          />
+
+          {loading ? (
+            <Grid container justifyContent="center" alignItems="center" style={{ height: "50vh" }}>
+              <CircularProgress sx={{ color: '#2196f3' }} />
+            </Grid>
+          ) : (
+            <>
+              <Grid container spacing={2} sx={{ mt: 2 }}>
+                {offers.map((offer) => (
+                  <Grid item xs={12} md={6} lg={4} key={offer.id}>
+                    <Card sx={{ p: 2 }} onClick={() => handleCardClick(offer.id)}>
+                      <Typography variant="h6">{offer.label}</Typography>
+                      <hr />
+                      <Typography variant="body2">
+                        <strong>Entreprise :</strong> {offer.company}
+                      </Typography>
+                      <Typography variant="body2">
+                        <strong>Compétences :</strong> {offer.skills}
+                      </Typography>
+                      <Typography variant="body2">
+                        <strong>Type de contrat :</strong> {offer.contract}
+                      </Typography>
+                      <Typography variant="body2">
+                        <strong>Ville :</strong> {offer.city}
+                      </Typography>
+                      <Typography variant="body2">
+                        <strong>Date de publication :</strong> {offer.publicationdate}
+                      </Typography>
+                      <Typography variant="body2">
+                        <strong>Date limite :</strong> {offer.deadlinedate}
+                      </Typography>
+                      <Typography variant="body2">
+                        <strong>Nombre de candidats :</strong> {offer.candidate ? offer.candidate.length : 0}
+                      </Typography>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+
+              <Pagination
+                count={totalPages}
+                page={page}
+                onChange={handlePageChange}
+                sx={{ mt: 4, display: "flex", justifyContent: "center" }}
+                color="primary"
+              />
+            </>
+          )}
         </Grid>
       </Grid>
     </Container>
--- src/layouts/sections/navigation/suggestion/components/index.js
+++ src/layouts/sections/navigation/suggestion/components/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Container, Grid, AppBar ,Card, Typography } from "@mui/material";
+import { Container, Grid, AppBar, Card, Typography, CircularProgress } from "@mui/material";
 import axios from "axios";
 import { API_URL } from "constants/coonstant.";
 import { useNavigate } from "react-router-dom";
@@ -7,23 +7,26 @@ import { useNavigate } from "react-router-dom";
 function SuggestionOffrePage() {
   const navigate = useNavigate();
   const [offers, setOffers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const numETU  = localStorage.getItem("numETU");
+  const numETU = localStorage.getItem("numETU");
 
   const fetchApplications = async () => {
     try {
       const response = await axios.get(`${API_URL}/student/list/recommandation/${numETU}`);
-      console.log(response.data.studentData.recommendations)
+      console.log(response.data.studentData.recommendations);
       const data = response.data.studentData.recommendations;
       setOffers(data);
     } catch (error) {
       console.error("Erreur lors du chargement des candidatures :", error);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchApplications();
-  },[]);
+  }, []);
 
   const handleCardClick = (id) => {
     navigate(`/offre/${id}`);
@@ -33,23 +36,30 @@ function SuggestionOffrePage() {
     <Container>
       <Grid container spacing={3} justifyContent="center">
         <Grid item xs={12} lg={10}>
-          <AppBar position="static">
-          </AppBar>
-          <Grid container spacing={2} sx={{ mt: 2 }}>
-            {offers.map((offer) => (
-              <Grid item xs={12} md={6} lg={4} key={offer.id}>
-                <Card sx={{ p: 2 }} onClick={() => handleCardClick(offer.offer_id)}>
-                  <Typography variant="h6">{offer.label}</Typography>
-                  <hr></hr>
-                  <Typography variant="body2"><strong>Entreprise :</strong> {offer.entreprise}</Typography>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
+          <AppBar position="static"></AppBar>
+
+          {loading ? (
+            <Grid container justifyContent="center" alignItems="center" color="primary" style={{ height: "50vh"}}>
+              <CircularProgress sx={{ color: '#2196f3' }}/>
+            </Grid>
+          ) : (
+            <Grid container spacing={2} sx={{ mt: 2 }}>
+              {offers.map((offer) => (
+                <Grid item xs={12} md={6} lg={4} key={offer.id}>
+                  <Card sx={{ p: 2 }} onClick={() => handleCardClick(offer.offer_id)}>
+                    <Typography variant="h6">{offer.label}</Typography>
+                    <hr />
+                    <Typography variant="body2">
+                      <strong>Entreprise :</strong> {offer.entreprise}
+                    </Typography>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          )}
         </Grid>
       </Grid>
     </Container>
   );
 }
-
 export default SuggestionOffrePage;
